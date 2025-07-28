c1343016e56c9004b614a9906a575df15ce960fd
kbhavre78 <kbhavre78@gmail.com>
2025-07-28 16:21:26 +0530

    Completed Side-nav component

--- src/app/budget-planner/budget-planner-routing-module.ts
+++ src/app/budget-planner/budget-planner-routing-module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Login } from './login/login';
+import { SideNav } from './side-nav/side-nav';
 
 const routes: Routes = [
   {path: 'login', component: Login},
+  {path: 'side-nav', component: SideNav},
 ];
 
 @NgModule({
---
+++ src/app/budget-planner/side-nav/side-nav.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-side-nav',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './side-nav.html',
+  styleUrl: './side-nav.scss'
+})
+export class SideNav {
+  isSlideOut: boolean = true;
+
+  constructor(private router: Router){}
+
+  toggleSlideOut(): void {
+    this.isSlideOut = !this.isSlideOut;
+  }
+
+  onDash(){
+    this.router.navigate(['budget-planner/dashboard']);
+  }
+  onProfile(){
+    this.router.navigate(['budget-planner/profile']);
+  }
+  onHistory(){
+    this.router.navigate(['budget-planner/history']);
+  }
+  onLogout(){
+    this.router.navigate(['budget-planner/logout']);
+  }
+}
