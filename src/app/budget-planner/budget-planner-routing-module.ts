e7bb9159e0143e9c7cced28faecdcc465dc26b4d
kbhavre78 <kbhavre78@gmail.com>
2025-07-29 11:39:07 +0530

    Completed income page

--- src/app/budget-planner/budget-planner-routing-module.ts
+++ src/app/budget-planner/budget-planner-routing-module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { Login } from './login/login';
 import { SideNav } from './side-nav/side-nav';
 import { Dashboard } from './dashboard/dashboard';
+import { Income } from './income/income';
 
 const routes: Routes = [
   {path: 'login', component: Login},
   {path: 'side-nav', component: SideNav},
   {path: 'dashboard', component: Dashboard},
+  {path: 'income', component: Income},
 ];
 
 @NgModule({
